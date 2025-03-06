b0ddf1c19dbb0600cd084ef5fdbac0f60ee46787
FernandoTheDev <ferreirareiscabralfernando@gmail.com>
2025-03-06 13:25:20 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
-g++ farpy.cpp ./src/lexer/lexer.cpp ./src/parser/parser.cpp ./src/error/error.cpp -o farpy -O3 
+if [ ! -d "bin" ]; then
+    mkdir bin
+    echo "Bin dir created."
+fi
+
+if [ -e "bin/farpy" ]; then
+    rm -f bin/farpy
+    echo "Binary excluded."
+fi
+
+g++ farpy.cpp ./src/lexer/lexer.cpp ./src/parser/parser.cpp ./src/error/error.cpp -o bin/farpy -O3
+echo "Compiled."
