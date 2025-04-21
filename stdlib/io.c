b73f9e470c56f5fb24b1b2de361bb585db64c491
FernandoTheDev <ferreirareiscabralfernando@gmail.com>
2025-04-21 09:40:39 -0300

    Update.

--- stdlib/io.c
+++ stdlib/io.c
@@ -4,11 +4,3 @@ void print(char *message)
 {
     printf("%s", message);
 }
-
-char *read_line()
-{
-    char *line = NULL;
-    size_t len = 0;
-    getline(&line, &len, stdin);
-    return line;
-}
